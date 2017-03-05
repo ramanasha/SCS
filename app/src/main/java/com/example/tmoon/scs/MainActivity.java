f918e5e2e2f669c7a4d73c7ed45936957afe3b9e
Tyler Moon <moon.tyler@gmail.com>
2017-03-04 20:30:07 -0500

    Added Firebase database

--- app/src/main/java/com/example/tmoon/scs/MainActivity.java
+++ app/src/main/java/com/example/tmoon/scs/MainActivity.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     //TODO: Replace this when the Settings Activiy is done
     Shooter[] shooters = new Shooter[4];
     int shooterIndex = 0;
+
+    int roundNumber = 0;
 
     Stations stations = new Stations(0);
 
@@ -51,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
         stationCounter.setText(Integer.toString(stations.getStationNumber()));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
     /*
      * This method is fired when the Hit button is pressed. It Increments the hit counter for the
      * current shooter at the current station number index.
@@ -67,10 +82,14 @@ public class MainActivity extends AppCompatActivity {
      * This method is fired when the Next Stations button is pressed. It increments the station
      * counter and updates the text fields
      */
-    public void nextStationButtonPressed(View view){
+    public void nextStationButtonPressed(MenuItem item){
         stations.incrementStationNumber();
         stationCounter.setText(Integer.toString(stations.getStationNumber()));
         hitCounter.setText(Integer.toString(shooters[shooterIndex].incrementHits(stations.getStationNumber())));
+    }
+
+    public void nextRoundButtonPressed(MenuItem item){
+        roundNumber++;
     }
 
     /*
@@ -79,53 +98,30 @@ public class MainActivity extends AppCompatActivity {
      * name.
      */
     public void nextShooterButtonPressed(View view){
+        // TODO: Possibly dont save every time this is pressed if it slows it down or something
+        saveData();
+
         incrementShooterIndex();
         shooterName.setText(shooters[shooterIndex].getName());
         hitCounter.setText(Integer.toString(shooters[shooterIndex].getHits()[stations.getStationNumber()]));// Reset the hit counter for the new shooter
 
-        // TODO: Possibly dont save every time this is pressed if it slows it down or something
-        saveData();
+
+
+
     }
 
     private void saveData(){
-
-        String filename = "SCSAnalysisData.csv";
-        String string = "Hello World!";
-
-        File path = getFilesDir();
-        File file = new File(path, filename);
-        System.out.println(file.getAbsolutePath().toString());
-        FileOutputStream stream;
         try {
-            stream = new FileOutputStream(file);
-            stream.write(string.getBytes());
-            stream.close();
-        } catch(IOException ie){
-            ie.printStackTrace();
-        }
 
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("Round " + Integer.toString(roundNumber));
+            myRef.child(shooters[shooterIndex].getName())
+                    .child("Station " + stations.getStationNumber())
+                    .child("Hits").setValue(shooters[shooterIndex].getHits()[stations.getStationNumber()]);
 
-
-
-
-        /*PrintWriter csvWriter;
-        String filePath;
-        try {
-            String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-            String fileName = "SCSAnalysisData.csv";
-            filePath = baseDir + File.separator + fileName;
-            System.out.println("Writing to file " + fileName);
-            File file = new File(filePath);
-            if (!file.exists())
-                file = new File(filePath);
-            csvWriter = new PrintWriter(new FileWriter(file,true));
-            csvWriter.print("Testing");
-            csvWriter.close();
-        }catch(IOException ie){
-            ie.printStackTrace();
         }catch(Exception e){
             e.printStackTrace();
-        }*/
+        }
 
     }
 
